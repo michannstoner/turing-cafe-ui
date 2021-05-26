199390e7b4745a084c2519da669e78b00bbf4c54
Your Name <stoner.michann@gmail.com>
2021-05-26 09:33:10 -0600

    Create method to store fetched data in state

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getAllReservations } from '../apiCalls'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      allReservations: []
+    }
+  }
+
+  componentDidMount = () => {
+    getAllReservations()
+      .then(data => this.setState({ allReservations: data }))
+  }
   render() {
+    console.log(this.state.allReservations);
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
