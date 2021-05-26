ad5a340304bf16356aed5072aa2d8cbbeb8f4798
Your Name <stoner.michann@gmail.com>
2021-05-26 10:35:31 -0600

    Add handleChange method to each input

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -7,11 +7,16 @@ class Form extends Component {
       name: '',
       date: '',
       time: '',
-      numberOfGuests: 0
+      numberOfGuests: null
     }
   }
 
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value} )
+  }
+
   render() {
+    console.log(this.state);
     return (
       <form className='resForm'>
         <input 
@@ -19,18 +24,21 @@ class Form extends Component {
           type='text'
           placeholder='Name'
           value={this.state.name}
+          onChange={event => this.handleChange(event)}
         />
         <input
           name='date'
           type='text'
           placeholder='Date (mm/dd)'
           value={this.state.date}
+          onChange={event => this.handleChange(event)}
         />
         <input 
           name='time'
           type='text'
           placeholder='Time'
           value={this.state.time}
+          onChange={event => this.handleChange(event)}
         />
         <input 
           name='numberOfGuests'
@@ -38,6 +46,7 @@ class Form extends Component {
           placeholder='Number of guests'
           value={this.state.numberofGuests}
           min='1'
+          onChange={event => this.handleChange(event)}
         />
         <button className='makeResButton'>Make Reservation</button>
       </form>
