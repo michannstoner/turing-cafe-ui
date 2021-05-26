3073f86e8f243b7ec878a9f5b335675546883a47
Your Name <stoner.michann@gmail.com>
2021-05-26 10:22:18 -0600

    Create basic class component structure

---
+++ src/Form/Form.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      numberOfGuests: 0
+    }
+  }
+
+  render() {
+    return (
+      <h1>hello</h1>
+    )
+  }
+}
+export default Form 
