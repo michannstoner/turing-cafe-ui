ec81f47187edc3ea237852a6b4bf6b6a9243ff6e
Your Name <stoner.michann@gmail.com>
2021-05-26 10:22:07 -0600

    Add Form component to render

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { getAllReservations } from '../apiCalls'
 import Reservations from '../Reservations/Reservations'
+import Form from '../Form/Form'
 
 class App extends Component {
   constructor() {
@@ -21,6 +22,7 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
+          <Form />
         </div>
         <div className='resy-container'>
           <Reservations reservations={this.state.allReservations} />
