607bae76a4844b269f3d6f8954cc0a3a76dfc8f8
Your Name <stoner.michann@gmail.com>
2021-05-26 09:47:00 -0600

    Create basic functional component in Reservations

---
+++ src/Reservations/Reservations.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from '../Card/Card'
+import './Reservations.css'
+
+const Reservations = ( { reservations }) => {
+  console.log(reservations);
+  return (
+    <h1>hello</h1>
+  )
+}
+
+export default Reservations
